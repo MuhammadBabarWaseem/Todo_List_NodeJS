4c147eebe87e5cbd873a42cb5ff65c91b66590e1
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-03-14 15:35:59 +0500

    created a todoList

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.get("/", (request, response) => {
   return response.send("Hello World!");
 });
 
+app.get("/todos", (request, response) => {
+    fs.readFile('./store/todo.json', 'utf-8', (err, data) => {
+        if (err) {
+            return response.status(500).send('Sorry, something wnet wrong.')
+        }
+
+        const todos = JSON.parse(data)
+        return response.json({todos: todos})
+    })
+});
+
 app.listen(3000, () => {
-    console.log('Application Running on http://localhost:3000')
+  console.log("Application Running on http://localhost:3000");
 });
